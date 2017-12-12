1ec0e0a0d06c2bce1f5da54a9256d6476de6d090
zengyan.bu <zengyan.bu@baozu.com>
2017-12-12 17:57:41 +0800

    公用label

---
+++ StudentInfoDemo/common/CommonLabel.h
@@ -0,0 +1,13 @@
+//
+//  CommonLabel.h
+//  StudentInfoDemo
+//
+//  Created by zengyan.bu on 2017/12/12.
+//  Copyright © 2017年 zengyan.bu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CommonLabel : UILabel
+
+@end
---
+++ StudentInfoDemo/common/CommonLabel.m
@@ -0,0 +1,18 @@
+//
+//  CommonLabel.m
+//  StudentInfoDemo
+//
+//  Created by zengyan.bu on 2017/12/12.
+//  Copyright © 2017年 zengyan.bu. All rights reserved.
+//
+
+#import "CommonLabel.h"
+
+@implementation CommonLabel
+- (instancetype)init{
+    if (self = [super init]) {
+    
+    }
+    return self;
+}
+@end
